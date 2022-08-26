32cb8842d4d158733a71e3f1e5c70c9627d80a43
sneezyG <ahmadgbolly97@gmail.com>
2022-08-26 20:01:48 +0100

    half completed exer

---
+++ exercise/TestTax.java
@@ -0,0 +1,28 @@
+
+
+class TestTax {
+  public static void main(String[] args) >{
+    
+    
+  }
+}
+
+class  Tax {
+  /* 
+  this tax class calculates a regular tax for an average Nigeria citizen.
+  */
+  string grossincome;
+  
+  
+  
+  
+}
+
+class extent FedaralTax {
+  /* 
+  this subclass FedaralTax calculates a tax for a Nigeria citizen that is a federal worker. there is an additional tax for fedaral workers as they enjoy federal benefits.
+  */
+  
+  
+  
+}
