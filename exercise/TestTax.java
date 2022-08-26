31774c6402bbeab597f76769dca1635cbdc7c121
sneezyG <ahmadgbolly97@gmail.com>
2022-08-26 23:54:31 +0100

    first headache

--- exercise/TestTax.java
+++ exercise/TestTax.java
@@ -1,28 +1,76 @@
 
 
-class TestTax {
-  public static void main(String[] args) >{
-    
-    
+class TestTax{
+  
+  /*
+  this create an instance of a Nigerian worker, output both the taxed income and federal taxed income for the worker.
+  */
+  
+  public static void main(String[] args) {
+   FederalWorker workerA = new FederalWorker();
+   workerA.grossIncome = 500000;
+   
+   double taxedIncome = workerA.incomeCalculator();
+   double fedTaxedIncome = workerA.incomeCalculator(taxedIncome);
+   
+   System.out.println("workerA taxed-income is : " + taxedIncome);
+   System.out.println("workerA federal-taxed-income is : " + fedTaxedIncome);
+   
   }
 }
 
-class  Tax {
+
+
+
+class Worker{
+  
   /* 
-  this tax class calculates a regular tax for an average Nigeria citizen.
+  this Worker class calculates the taxed-income for an average Nigerian worker based on their gross income.
   */
-  string grossincome;
   
+  double grossIncome;
+  final String nationality = "Nigeria";
   
-  
+  public double incomeCalculator() {
+    // this return the taxedincome
+    if (grossIncome <= 30000) {
+      return grossIncome;
+    }
+    else if (grossIncome > 30000 && grossIncome <= 60000) {
+      return grossIncome - (grossIncome * (5/100));
+    }
+    else if (grossIncome > 60000 && grossIncome <= 100000) {
+      return grossIncome - (grossIncome * (7/100));
+    }
+    else if (grossIncome > 100000 && grossIncome <= 500000) {
+      return grossIncome - (grossIncome * (9/100));
+    }
+    else{
+      return grossIncome - (grossIncome * (15/100));
+    }
+   
+  } 
   
 }
 
-class extent FedaralTax {
+
+
+
+class FederalWorker extends Worker{
+  
   /* 
-  this subclass FedaralTax calculates a tax for a Nigeria citizen that is a federal worker. there is an additional tax for fedaral workers as they enjoy federal benefits.
+  this FederalWorker class is a subclass of Worker class, have an additional method that calculates the federal-Taxed-Income for a Nigerian federal worker from the taxed-income
   */
   
+  public double incomeCalculator(double taxedIncome) {
+    // this return the fedTaxedIncome.
+    if (grossIncome > 100000) {
+      return taxedIncome - (taxedIncome * (5/100));
+    }
+    else{
+      return taxedIncome;
+    }
   
+  }
   
 }
